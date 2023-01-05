3e6aef466477fe10e8b545de05b9c46b6f933037
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-01-05 13:51:40 +0100

    Added Contact Page to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
                 
                 <Route path="*" element={fallback()} />
             </Routes>
