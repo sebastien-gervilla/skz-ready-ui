3695c8bc6b6683fbc6aad521020c11c0fc4c9017
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2023-01-06 13:10:46 +0100

    Added translations to ContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,19 +3,23 @@ import { BsFacebook, BsFillTelephoneFill, BsInstagram } from 'react-icons/bs';
 import { MdMail } from 'react-icons/md';
 import { HiLocationMarker } from 'react-icons/hi';
 import { FaLinkedinIn } from 'react-icons/fa';
+import useTranslate from '../hooks/useTranslate';
 
 const ContactForm = () => {
+
+    const translate = useTranslate('contact.form.');
+
     return (
         <div className="contact-form app-box box-row">
-            <ContactInformations />
+            <ContactCard />
             <form className='app-form'>
                 <div className="form-input_row">
                     <div className="form-input">
-                        <p>First name</p>
+                        <p>{translate('firstName')}</p>
                         <input placeholder='David' type="text" name='fname' />
                     </div>
                     <div className="form-input">
-                        <p>Last name</p>
+                        <p>{translate('lastName')}</p>
                         <input placeholder='Jones' type="text" name='lname' />
                     </div>
                 </div>
@@ -25,19 +29,19 @@ const ContactForm = () => {
                         <input placeholder='myemail@exemple.com' type="text" name='email' />
                     </div>
                     <div className="form-input">
-                        <p>Phone</p>
+                        <p>{translate('phone')}</p>
                         <input placeholder='06 22 81 44 65' type="text" name='phone' />
                     </div>
                 </div>
                 <div className="form-input">
                     <p>Message</p>
-                    <textarea name="message" placeholder="Hi, i'm contacting you to let you know...">
+                    <textarea name="message" placeholder={translate('messagePlaceholder')}>
 
                     </textarea>
                 </div>
                 <div className="form-input submit">
                     <button className='animated' type='submit'>
-                        Send Message
+                        {translate('submit')}
                     </button>
                 </div>
             </form>
@@ -45,12 +49,15 @@ const ContactForm = () => {
     );
 };
 
-const ContactInformations = () => {
+const ContactCard = () => {
+
+    const translate = useTranslate('contact.card.');
+
     return (
-        <div className="contact-infos">
+        <div className="contact-card">
             <div className="header">
-                <h4>Contact Informations</h4>
-                <p>Fill this form and we'll come back to you as soon as possible.</p>
+                <h4>{translate('title')}</h4>
+                <p>{translate('desc')}</p>
             </div>
             <div className="infos">
                 <div className="row">
